403779a57ac1a15b7456d6c26efafdea76da7f9a
Recep Özgür MIH <120413480+recepzgrmh@users.noreply.github.com>
2023-07-12 15:53:20 +0300

    button animation

--- troll/index.js
+++ troll/index.js
@@ -1,9 +1,20 @@
 function Event(){
     var sound = new Audio("audio/"+this.innerText+".mp3");
     sound.play();
+    buttonAnimation(this.innerText);
+}   
+
+function buttonAnimation(pressed){
+    var currentKey = document.querySelector("." + pressed);
+
+    currentKey.classList.add("change");
+
+    const myTimeout = setTimeout(press, 100);
+
+    function press() {
+        currentKey.classList.remove("change");
+    }
 }
-
-
 
 for(var i = 0 ; i < document.querySelectorAll(".display-grid").length ; i++){
     document.querySelectorAll(".display-grid")[i].addEventListener("click",Event);
