c3cf7b4e79396f2d8f8d3b4381503e8b45d4490e
Recep Özgür MIH <120413480+recepzgrmh@users.noreply.github.com>
2023-07-16 17:18:55 +0300

    Update script.js

--- Random Number Website/script.js
+++ Random Number Website/script.js
@@ -1,9 +1,4 @@
-let randomNumber = Math.floor(Math.random()*100+1);
-
-
-document.querySelector(".number").innerHTML = randomNumber;
-
-
-document.querySelector(".button").addEventListener("click",function(){
+document.querySelector("button").addEventListener("click",function(){
+    let randomNumber = Math.floor(Math.random()*100+1);
     document.querySelector(".number").innerHTML = randomNumber;
 })
